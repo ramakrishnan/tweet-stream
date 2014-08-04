f3bea284f769e3ab0011eba6d2913eab4be3b88b
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-08-04 17:19:07 +0530

    Add reference for Geo co-ordinates calculations

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,15 @@
+// Reference for Geo co-ordinates conversion
+// http://en.wikipedia.org/wiki/Geographic_coordinate_conversion
 var TSamples = function() {
     var writeAddress = function(coordinates) {
-        var latLong = coordinates[0].toString() + ',' + coordinates[1].toString();
-        var latlng = new google.maps.LatLng(coordinates[0], coordinates[1]);
+        var latLong = coordinates.lati.toString() + ',' + coordinates.longi.toString();
+        var latlng = new google.maps.LatLng(coordinates.lati, coordinates.longi);
         geocoder.geocode({'latLng': latlng}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
+                console.log(status);
                 if (results[1]) {
                     address = results[1].formatted_address;
+                    console.log(address);
                     var node = $('div[data-latlong="'+ latLong + '"]');
                     node.append($('<span>').text(address).addClass('address'));
                 }
@@ -15,8 +19,14 @@ var TSamples = function() {
 
     var initSocker = function() {
         var socket = io.connect(window.location.hostname);
+        var coordinates = { lati: 0, longi: 0};
         socket.on('data', function(data) {
-            var latLong = data.coordinates[0].toString() + ',' + data.coordinates[1].toString();
+            // coordinates.lati  = Math.round(data.coordinates[0]; * 100) / 100;
+            // coordinates.longi  = Math.round(data.coordinates[1]; * 100) / 100;
+            coordinates.lati  = data.coordinates[0];
+            coordinates.longi  = data.coordinates[1];
+
+            var latLong = coordinates.lati.toString() + ',' + coordinates.longi.toString();
             var node = $('div[data-latlong="'+ latLong + '"]');
             var address = '';
             if (node.length > 0) {
@@ -28,7 +38,7 @@ var TSamples = function() {
                     .append($('<span>').text(1).addClass('count'))
                     .append($('<span>').text(latLong).addClass('caption'));
                 $('div.tweet_location').append(node);
-                writeAddress(data.coordinates);
+                writeAddress(coordinates);
             }
         });
     };
